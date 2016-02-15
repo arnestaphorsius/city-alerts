328a3c2c348c0304dc2194e5f9fca40939b7ddee
Arne Staphorsius <arne.staphorsius@incentro.com>
2016-02-15 16:28:16 +0100

    Add Endpoint for local startup

--- src/main/java/com/incentro/BerichtMeldingPublisher.java
+++ src/main/java/com/incentro/BerichtMeldingPublisher.java
@@ -1,6 +1,7 @@
 package main.java.com.incentro;
 
 import main.java.com.incentro.core.util.App;
+import main.java.com.incentro.ws.services.impl.DataRequestImpl;
 import main.java.com.incentro.ws.services.impl.DataResponseImpl;
 import main.java.com.incentro.ws.services.impl.ServiceSoapImpl;
 import main.java.com.incentro.ws.services.impl.StatusResponseImpl;
@@ -20,6 +21,7 @@ public class BerichtMeldingPublisher {
   public static void main(String... args) {
     Endpoint.publish("http://127.0.0.1:8088/ServiceSoap", new ServiceSoapImpl());
     Endpoint.publish("http://127.0.0.1:8088/StatusResponse", new StatusResponseImpl());
+    Endpoint.publish("http://127.0.0.1:8088/DataRequest", new DataRequestImpl());
     Endpoint.publish("http://127.0.0.1:8088/DataResponse", new DataResponseImpl());
   }
 }
