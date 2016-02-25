fbc590b45938adc739bec7fd5389bb60d9e81f25
Arne Staphorsius <arne.staphorsius@incentro.com>
2016-02-25 16:17:31 +0100

    Remove unused imports.

--- src/main/java/com/incentro/ws/services/impl/DataResponseImpl.java
+++ src/main/java/com/incentro/ws/services/impl/DataResponseImpl.java
@@ -1,12 +1,9 @@
 package main.java.com.incentro.ws.services.impl;
 
-import main.java.com.incentro.core.controllers.Brandweer;
 import main.java.com.incentro.core.util.App;
 import main.java.com.incentro.ws.models.da.IncomingDoc;
 import main.java.com.incentro.ws.models.da.ResultDoc;
 import main.java.com.incentro.ws.services.DataResponse;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
