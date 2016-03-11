493c8b36d137a9034a69fb53c311aa63921befad
Arne Staphorsius <arne.staphorsius@incentro.com>
2016-03-11 14:47:33 +0100

    Add init and constants for StatusResponse database.

--- src/main/java/com/incentro/core/util/App.java
+++ src/main/java/com/incentro/core/util/App.java
@@ -20,54 +20,29 @@ public class App {
   private static Logger log = LogManager.getLogger(App.class);
 
   private static Properties PROPERTIES = null;
-  private static BasicDataSource CONNECTION_POOL = null;
+
+  private static BasicDataSource DATA_REQUEST_CONNECTION_POOL = null;
+  private static BasicDataSource STATUS_RESPONSE_CONNECTION_POOL = null;
 
   public static void init() {
 
     // Load all properties
     if (PROPERTIES == null) Props.init();
 
-    // Set up connection to the database
-    if (CONNECTION_POOL == null) DBConn.init();
+    // Set up connection to the databases
+    DBConn.init();
   }
 
   public static String getProperty(String key) {
     return PROPERTIES.getProperty(key);
   }
 
-  public static Connection getConnection() throws SQLException {
-    return CONNECTION_POOL.getConnection();
+  public static Connection getDataRequestConnection() throws SQLException {
+    return DATA_REQUEST_CONNECTION_POOL.getConnection();
   }
 
-  private static class DBConn {
-
-    protected static void init() {
-
-      try {
-        Class.forName(getProperty(Constants.Database.DB_DRIVER));
-      } catch (ClassNotFoundException e) {
-        log.error("PostgreSQL JDBC Driver not found.", e);
-        return;
-      }
-
-      try {
-        CONNECTION_POOL = new BasicDataSource();
-
-        CONNECTION_POOL.setUsername(getProperty(Constants.Database.DB_USER));
-        CONNECTION_POOL.setPassword(getProperty(Constants.Database.DB_PASSWORD));
-        CONNECTION_POOL.setUrl(getProperty(Constants.Database.DB_URL));
-        CONNECTION_POOL.setDriverClassName(getProperty(Constants.Database.DB_DRIVER));
-
-        CONNECTION_POOL.setInitialSize(3);
-
-        // Retrieve and close a connection to test the configuration.
-        Connection connection = getConnection();
-        connection.close();
-
-      } catch (SQLException e) {
-        log.error("Unable to connect to the PostgreSQL database.");
-      }
-    }
+  public static Connection getStatusResponseConnection() throws SQLException {
+    return STATUS_RESPONSE_CONNECTION_POOL.getConnection();
   }
 
   private static class Props {
@@ -92,6 +67,61 @@ public class App {
           } catch (IOException e) {
             e.printStackTrace();
           }
+        }
+      }
+    }
+  }
+
+  private static class DBConn {
+
+    protected static void init() {
+
+      try {
+        Class.forName(getProperty(Constants.Database.DB_DRIVER));
+      } catch (ClassNotFoundException e) {
+        log.error("PostgreSQL JDBC Driver not found.", e);
+        return;
+      }
+
+      if (DATA_REQUEST_CONNECTION_POOL == null) {
+
+        try {
+          DATA_REQUEST_CONNECTION_POOL = new BasicDataSource();
+
+          DATA_REQUEST_CONNECTION_POOL.setUsername(getProperty(Constants.DataRequest.DB_USER));
+          DATA_REQUEST_CONNECTION_POOL.setPassword(getProperty(Constants.DataRequest.DB_PASSWORD));
+          DATA_REQUEST_CONNECTION_POOL.setUrl(getProperty(Constants.DataRequest.DB_URL));
+          DATA_REQUEST_CONNECTION_POOL.setDriverClassName(getProperty(Constants.Database.DB_DRIVER));
+
+          DATA_REQUEST_CONNECTION_POOL.setInitialSize(3);
+
+          // Retrieve and close a connection to test the configuration.
+          Connection connection = getDataRequestConnection();
+          connection.close();
+
+        } catch (SQLException e) {
+          log.error("Unable to connect to the DataRequest PostgreSQL database.");
+        }
+      }
+
+      if (STATUS_RESPONSE_CONNECTION_POOL == null) {
+
+        try {
+          STATUS_RESPONSE_CONNECTION_POOL = new BasicDataSource();
+
+          STATUS_RESPONSE_CONNECTION_POOL.setUsername(getProperty(Constants.StatusResponse.DB_USER));
+          STATUS_RESPONSE_CONNECTION_POOL.setPassword(getProperty(Constants.StatusResponse.DB_PASSWORD));
+          STATUS_RESPONSE_CONNECTION_POOL.setUrl(getProperty(Constants.StatusResponse.DB_URL));
+          STATUS_RESPONSE_CONNECTION_POOL.setDriverClassName(getProperty(Constants.Database.DB_DRIVER));
+
+          STATUS_RESPONSE_CONNECTION_POOL.setInitialSize(3);
+
+          // Retrieve and close a connection to test the configuration.
+          Connection connection = getStatusResponseConnection();
+          connection.close();
+
+        } catch (SQLException e) {
+          log.error("Unable to connect to the StatusResponse PostgreSQL database.");
         }
       }
     }
--- src/main/java/com/incentro/core/util/Constants.java
+++ src/main/java/com/incentro/core/util/Constants.java
@@ -14,18 +14,21 @@ public class Constants {
   public static class Database {
 
     public static final String DB_DRIVER = "db.driver";
-    public static final String DB_URL = "db.url";
-    public static final String DB_USER = "db.user";
-    public static final String DB_PASSWORD = "db.password";
   }
 
   public static class DataRequest {
 
     public static final String DB_TABLE = "db.dr.table";
+    public static final String DB_URL = "db.dr.url";
+    public static final String DB_USER = "db.dr.user";
+    public static final String DB_PASSWORD = "db.dr.password";
   }
 
   public static class StatusResponse {
 
-    public static final String DB_TABLE = "db.ds.table";
+    public static final String DB_TABLE = "db.sr.table";
+    public static final String DB_URL = "db.sr.url";
+    public static final String DB_USER = "db.sr.user";
+    public static final String DB_PASSWORD = "db.sr.password";
   }
 }
