04de106df696b4618f5a99cc705ce0934ad68c0f
Arne Staphorsius <arne.staphorsius@incentro.com>
2016-03-29 12:56:07 +0200

    Remove BerichtMelding from the project

--- src/main/java/com/incentro/BerichtMeldingPublisher.java
+++ src/main/java/com/incentro/BerichtMeldingPublisher.java
@@ -3,7 +3,6 @@ package main.java.com.incentro;
 import main.java.com.incentro.core.util.App;
 import main.java.com.incentro.ws.services.impl.DataRequestImpl;
 import main.java.com.incentro.ws.services.impl.DataResponseImpl;
-import main.java.com.incentro.ws.services.impl.ServiceSoapImpl;
 import main.java.com.incentro.ws.services.impl.StatusResponseImpl;
 
 import javax.xml.ws.Endpoint;
@@ -19,7 +18,6 @@ public class BerichtMeldingPublisher {
   }
 
   public static void main(String... args) {
-    Endpoint.publish("http://127.0.0.1:8080/city-alerts/ServiceSoap", new ServiceSoapImpl());
     Endpoint.publish("http://127.0.0.1:8080/city-alerts/StatusResponse", new StatusResponseImpl());
     Endpoint.publish("http://127.0.0.1:8080/city-alerts/DataRequest", new DataRequestImpl());
     Endpoint.publish("http://127.0.0.1:8080/city-alerts/DataResponse", new DataResponseImpl());
--- src/main/java/com/incentro/ws/models/bm/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:8088/ServiceSoap/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package main.java.com.incentro.ws.models.bm;
